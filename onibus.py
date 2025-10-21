4d093be5528f0f73d8f378df88680e16490abf22
Samuel Andrey <samuelandrey193@gmail.com>
2025-10-21 18:02:12 -0300

    Código bruto do projeto

--- onibus.py
+++ onibus.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from folium import Map, Marker
 from dotenv import load_dotenv
 
 load_dotenv(".env") # o nome dentro do parenteses deve ser o nome do arquivo onde sua chave de API está salva
@@ -8,4 +9,19 @@ s = requests.Session()
 res = s.post(
     f"http://api.olhovivo.sptrans.com.br/v2.1/Login/Autenticar?token={os.getenv('SPTRANS_TOKEN')}"
 )
-print(res.text)
+
+res = s.get(
+    "http://api.olhovivo.sptrans.com.br/v2.1/Parada/BuscarParadasPorLinha?codigoLinha=2506"
+)
+paradas = res.json()
+paradas[:3]
+
+m = Map(location=[paradas[3]["py"], paradas[3]["px"]], zoom_start=14)
+for i in paradas:
+    Marker(location=[i["py"], i["px"]], popup=i["np"]).add_to(m)
+#m.show_in_browser()
+
+pos = s.get(
+    "http://api.olhovivo.sptrans.com.br/v2.1//Posicao/Linha?codigoLinha=2506"
+)
+pos.json()
